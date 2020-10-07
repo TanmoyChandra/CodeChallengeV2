e146b00dc2eb210bfd1c11e3fa837bfeb9bc3632
tanmoychandra <chandratanmoy5@gmail.com>
2020-10-07 20:18:43 +0530

    built in war

--- src/main/java/com/example/demo/FirstProjectApplication.java
+++ src/main/java/com/example/demo/FirstProjectApplication.java
@@ -3,13 +3,14 @@ package com.example.demo;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import ch.qos.logback.classic.Logger;
 //import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-public class FirstProjectApplication {
+public class FirstProjectApplication extends SpringBootServletInitializer{
 
 	private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(FirstProjectApplication.class);
 	
